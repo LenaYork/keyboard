f6fbc20d4f71dfa33afeb57a8fcfa424578986c1
LenaYork <lenayork@tut.by>
2022-04-24 19:31:10 +0300

    fix: correct mobile menu bar on pets page

--- shelter/pages/pets/main.js
+++ shelter/pages/pets/main.js
@@ -1,6 +1,30 @@
+// const mobileMenu = document.querySelector("#mobile-menu");
+// mobileMenu.addEventListener("click", function() {
+//     mobileMenu.classList.toggle("active");
+//     document.querySelector("#header").classList.toggle("mobile-bar");
+//     // document.body.classList.toggle(".overflow");
+// })
+
+let mobileBar; 
 const mobileMenu = document.querySelector("#mobile-menu");
-mobileMenu.addEventListener("click", function() {
+
+function toggleMenu() {
     mobileMenu.classList.toggle("active");
     document.querySelector("#header").classList.toggle("mobile-bar");
-    // document.body.classList.toggle(".overflow");
+    document.body.classList.toggle("overflow");
+    document.querySelector("#wrapper").classList.toggle("inactive-bg");
+    document.querySelector("#fade-wrapper").classList.toggle("fade-wrapper");
+}
+
+mobileMenu.addEventListener("click", function(e) {
+    toggleMenu();
+    mobileBar = document.querySelector(".mobile-bar");
+    e.stopPropagation();
+})
+
+document.addEventListener("click", function(event) {
+    if (mobileBar && (!event.path.includes(mobileBar) || event.target.classList.contains("header-nav-link"))) {
+        toggleMenu();
+        mobileBar = null;
+    }
 })
